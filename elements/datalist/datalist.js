a81c65fc15f3c1542f3d9d924bed4b1c11fc370e
Alexander Seppälä <dev@timot.se>
2014-12-15 11:53:00 +0100

    use "runtime" to load element

--- elements/datalist/datalist.js
+++ elements/datalist/datalist.js
@@ -1,27 +1,55 @@
 'use strict';
 
-import {list_item} from '../../common/renderer';
+import {list_item as render_list_item} from '../../common/renderer';
+
+let _data = '_data_', _document = '_document_';
+
 
 class DataList extends window.HTMLDataListElement {
 	set data(value) {
+		let nodelist;
 		if ( Array.isArray(value) !== true ) {
 			throw new Error('data has to be an array');
 		}
 
-		this.list = value;
-		this.selected = -1;
+		this[_data].data = value;
+		this[_data].selected = -1;
 
-		if ( this.is_attached === true ) {
-			this.render();
+		nodelist = window.document.createDocumentFragment();
+
+		if ( this.data.length === 0 ) {
+			this.classList.add('empty');
+		} else {
+			this.classList.remove('empty');
+		}
+
+		if ( this[_data].is_attached === true ) {
+			this.data.forEach(item => {
+				nodelist.appendChild(render_list_item(this[_data].tpl, item));
+			});
+
+			Array.prototype.forEach.call(this.querySelectorAll('tt-datalist > *:not(template'), node => {
+				this.removeChild(node);
+			});
+
+			this.appendChild(nodelist);
 		}
 	}
 
 	get data() {
-		return this.list;
+		return this[_data].data;
+	}
+
+	set target(node) {
+		this[_data].target = node;
+	}
+
+	get target() {
+		return this[_data].target;
 	}
 
 	set value(value) {
-		this.selected = Array.prototype.reduce.call(this.querySelectorAll('tt-datalist > *:not(template'), (index, node, node_index) => {
+		this[_data].selected = Array.prototype.reduce.call(this.querySelectorAll('tt-datalist > *:not(template'), (index, node, node_index) => {
 			if ( value === node.value ) {
 				return node_index;
 			} else {
@@ -29,118 +57,102 @@ class DataList extends window.HTMLDataListElement {
 			}
 		}, -1);
 
-		this.render_selected();
+		render_selected(this);
 	}
 
 	get value() {
-		if ( this.selected === -1 ) {
+		if ( this[_data].selected === -1 ) {
 			return '';
 		} else {
-			return this.list[this.selected];
+			return this.data[this[_data].selected];
 		}
 	}
 
 	createdCallback() {
-		this.is_attached = false;
-		this.list = [];
-		this.selected = -1;
-		this.targets = window.Array.prototype.slice.call(window.document.querySelectorAll(this.getAttribute('target')), 0);
+		this[_data] = {};
+		this[_data].is_attached = false;
+		this.data = [];
+		this.target = window.document.querySelector(this.getAttribute('target'));
 
-		this.tpl = this.querySelector('template');
-		if ( !this.tpl ) {
-			this.tpl = this.document.querySelector('template');
+		this[_data].tpl = this.querySelector('template');
+		if ( !this[_data].tpl ) {
+			this[_data].tpl = this[_document].querySelector('template');
 		}
 	}
 
 	attachedCallback() {
-		this.is_attached = true;
-		this.render();
-		window.addEventListener('keydown', this.keydown_event.bind(this));
+		this[_data].is_attached = true;
+		this.data = this.data;
+		this.target.addEventListener('keydown', keydown_event.bind(this));
 	}
 
 	detachedCallback() {
-		window.removeEventListener('keydown', this.keydown_event.bind(this));
+		this.target.removeEventListener('keydown', keydown_event.bind(this));
 	}
 
-	render() {
-		let nodelist = document.createDocumentFragment();
-
-		if ( this.is_attached === true ) {
-			this.list.forEach(function(item) {
-				console.log(item);
-				nodelist.appendChild(list_item(this.tpl, item));
-			}.bind(this));
-
-			Array.prototype.forEach.call(this.querySelectorAll('tt-datalist > *:not(template'), function(node) {
-				this.removeChild(node);
-			}.bind(this));
-
-			this.appendChild(nodelist);
+	attributeChangedCallback(name, oldval, newval) {
+		if ( name === 'target' ) {
+			this.target = window.document.querySelector(newval);
 		}
 	}
+}
 
-	select_sibling(direction) {
-		let pos = this.selected + (direction==='previous'?-1:+1);
+function select_sibling(datalist, step) {
+	let pos = datalist[_data].selcted + step;
 
-		if ( pos < 0 ) {
-			pos = 0;
-		} else if ( pos >= this.list.length - 1 ) {
-			pos = this.list.length - 1 ;
+	if ( pos < 0 ) {
+		pos = 0;
+	} else if ( pos >= datalist.data.length - 1 ) {
+		pos = datalist.data.length - 1 ;
+	}
+
+	datalist[_data].selected = pos;
+	render_selected(datalist);
+}
+
+function render_selected(datalist) {
+	Array.prototype.forEach.call(datalist.children, (node, index) => {
+		if ( index === this[_data].selected ) {
+			node.classList.add('selected');
+		} else {
+			node.classList.remove('selected');
 		}
+	});
+}
 
-		this.selected = pos;
-		this.render_selected();
-	}
+function keydown_event(evt) {
+	let key = evt.keyCode;
 
-	render_selected() {
-		Array.prototype.forEach.call(this.children, function(node, index) {
-			if ( index === this.selected ) {
-				node.classList.add('selected');
-			} else {
-				node.classList.remove('selected');
-			}
-		}.bind(this));
-	}
+	if ( this.data.length > 0 ) {
+		if ( key === 27 ) {
+			this.data = [];
+			evt.preventDefault();
+		} else if ( key === 38 || ( key === 9 && evt.shiftKey ) ) {
+			evt.preventDefault();
+			select_sibling(this, -1);
+		} else if ( key === 40 || ( key === 9 && !evt.shiftKey ) ) {
+			evt.preventDefault();
+			select_sibling(this, 1);
+		} else if ( key === 13 ) {
+			if ( this[_data].selected >= 0 ) {
+				evt.preventDefault();
 
-	keydown_event(evt) {
-		this.targets.some(target => {
-			if ( target !== evt.target ) { return false; }
-			let key = evt.keyCode;
-
-			if ( this.list.length > 0 ) {
-				if ( key === 27 ) {
-					this.data = [];
-					evt.preventDefault();
-				} else if ( key === 38 || ( key === 9 && evt.shiftKey ) ) {
-					evt.preventDefault();
-					this.select_sibling('previous');
-				} else if ( key === 40 || ( key === 9 && !evt.shiftKey ) ) {
-					evt.preventDefault();
-					this.select_sibling('next');
-				} else if ( key === 13 ) {
-					if ( this.selected >= 0 ) {
-						evt.preventDefault();
-
-						if ( this.selected > -1 ) {
-							this.dispatchEvent(new CustomEvent('select', {
-								detail: {
-									value: this.list[this.selected],
-									target: target
-								},
-								bubbles: true,
-								cancelable: true
-							}));
-						}
-					}
+				if ( this[_data].selected > -1 ) {
+					this.dispatchEvent(new CustomEvent('select', {
+						detail: {
+							value: this.data[this[_data].selected],
+							target: this.target
+						},
+						bubbles: true,
+						cancelable: true
+					}));
 				}
 			}
-
-			return true;
-		});
+		}
 	}
 }
 
 export default function(document) {
-	DataList.prototype.document = document;
+	DataList.prototype[_document] = document;
 	window.document.registerElement('tt-datalist', {prototype: DataList.prototype});
 }
