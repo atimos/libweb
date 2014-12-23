651562169e4fcc9e2edd77197c9eeb0db7f467fd
Alexander Seppälä <dev@timot.se>
2014-12-23 16:44:50 +0100

    do not do unrelated stuff

--- common/renderer.js
+++ common/renderer.js
@@ -1,22 +1,7 @@
 'use strict';
 
-export function list_item(tpl, data) {
-	var element = tpl.content.cloneNode(true).firstElementChild;
-
-	let value;
-	if ( element.dataset.value !== undefined ) {
-		value = data[element.dataset.value];
-		delete element.dataset.value;
-	} else if ( data.value !== undefined ) {
-		value = data.value;
-	}
-
-	//TODO: serilize non-string objects
-	if ( value !== undefined ) {
-		element.value = data.value;
-	}
-	
-	Array.prototype.forEach.call(element.querySelectorAll('[data-key]'), function(node) {
+export function item(node, data) {
+	Array.prototype.forEach.call(node.querySelectorAll('[data-key]'), function(node) {
 		node.dataset.key.split('|').some(function(key) {
 			if ( data[key] !== undefined ) {
 				node.textContent = data[key];
@@ -26,6 +11,5 @@ export function list_item(tpl, data) {
 
 		delete node.dataset.key;
 	});
-
-	return element;
+	return node;
 }
