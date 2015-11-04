8238229e43530806c8a147994076ba59c2078af0
Alexander Seppälä <dev@timot.se>
2015-11-04 09:48:40 +0100

    changed indexeddb to work with rxjs

--- db/indexeddb.js
+++ db/indexeddb.js
@@ -1,101 +1,98 @@
 'use strict';
 
-let uuid = require('../lib/node-uuid/uuid.js', 'min'),
-	Promise = require('../lib/bluebird/bluebird.js'),
-	Stream = require('../lib/streamjs/stream.js');
+let uuid = require('../lib/node-uuid/uuid.js'),
+	Rx = require('../lib/rxjs/dist/rx.all.js');
 
-let _store = Symbol('store'),
-	_trans = Symbol('trans'),
-	_index = Symbol('index'),
-	_range = Symbol('range'),
-	_cfg = Symbol('config');
-
-export default function(name, version) {
-	let config_map = new Map();
+export default (db_name, db_version = null) => {
+	let db_cfg = new Map();
 
 	return {
-		set(name, config) {
-			if ( config.key_gen === 'increment' ) {
-				config.autoIncrement = true;
-				delete config.key_gen;
-			} else if ( config.key_gen === 'uuid' ) {
-				config.key_gen = uuid.v4;
-			} else if ( typeof config.key_gen !== 'function' ) {
-				delete config.key_gen;
+		store(name, store_cfg) {
+			if ( store_cfg.keyType === 'autoIncrement' ) {
+				store_cfg.autoIncrement = true;
 			}
 
-			if ( config.key ) {
-				config.keyPath = config.key;
-			}
+			db_cfg.set(name, store_cfg);
 
-			delete config.key;
-
-			config_map.set(name, config);
 			return this;
 		},
-		then: (resolve, reject) => {
+		then(resolve, reject) {
 			return new Promise((resolve, reject) => {
-				let request;
+				let db_request = (db_version === null ? window.indexedDB.open(db_name) : window.indexedDB.open(db_name, db_version));
 
-				if ( version !== undefined ) {
-					request = window.indexedDB.open(name, version);
-				} else {
-					request = window.indexedDB.open(name);
-				}
+				db_request.addEventListener('upgradeneeded', evt => {
+					for ( let [store_name, store_cfg] of db_cfg.entries() ) {
+						let store = null;
 
-				request.addEventListener('upgradeneeded', evt => {
-					for ( let entry of config_map.entries() ) {
-						let [name, config] = entry,
-							index = config.index || [],
-							store = null;
-
-						if ( evt.target.result.objectStoreNames.contains(name) ) {
-							evt.target.result.deleteObjectStore(name);
+						if ( evt.target.result.objectStoreNames.contains(store_name) ) {
+							evt.target.result.deleteObjectStore(store_name);
 						}
 
-						store = evt.target.result.createObjectStore(name, config);
+						store = evt.target.result.createObjectStore(store_name, store_cfg);
 
-						index
-							.forEach(config => {
-								store.createIndex(config.name, config.keyPath || config.name, config);
+						(store_cfg.index || [])
+							.forEach(index_cfg => {
+								store.createIndex(index_cfg.name, index_cfg.keyPath || index_cfg.name, index_cfg);
 							});
 					}
 				});
 
-				request.addEventListener('success', evt => {
+				db_request.addEventListener('success', evt => {
 					resolve({
-						has(store_name) {
-							return (evt.target.objectStoreNames[store_name] !== undefined);
-						},
-						get(store_list = [], mode = 'readonly') {
-							return new Promise(resolve => {
-								let transaction = evt.target.result.transaction(store_list, mode);
+						transaction(store_list = []) {
+							return Rx.Observable.create(observer => {
+								let transaction = evt.target.result.transaction(store_list, 'readonly');
 
-								let store_map = new Map();
+								transaction.addEventListener('error', evt => { observer.onError(evt.target.error); });
+								transaction.addEventListener('complete', observer.onCompleted.bind(observer));
 
-								store_list
-									.forEach(name => {
-										store_map.set(name, new Store(transaction.objectStore(name), config_map.get(name)));
-									});
-
-								resolve(store_map);
+								observer.onNext(store_list.map(name => { return Store.with_transaction(name, db_cfg.get(name), transaction); }));
 							});
+						},
+						transaction_mut(store_list = []) {
+							return Rx.Observable.create(observer => {
+								let transaction = evt.target.result.transaction(store_list, 'readwrite');
+
+								transaction.addEventListener('error', evt => { observer.onError(evt.target.error); });
+								transaction.addEventListener('complete', observer.onCompleted.bind(observer));
+
+								observer.onNext(store_list.map(name => { return StoreMut.with_transaction(name, db_cfg.get(name), transaction); }));
+							});
+						},
+						store(name) {
+							return Store.with_db(name, db_cfg.get(name), evt.target.result);
+						},
+						store_mut(name) {
+							return StoreMut.with_db(name, db_cfg.get(name), evt.target.result);
 						},
 						delete_database() {
 							return new Promise((resolve, reject) => {
-								var request = window.indexedDB.deleteDatabase(name);
+								let db_request = window.indexedDB.deleteDatabase(name);
 
-								request.addEventListener('success', resolve);
+								db_request.addEventListener('success', resolve);
 
-								request.addEventListener('error', evt => {
+								db_request.addEventListener('error', evt => {
 									reject(evt.target.error);
 								});
+							});
+						},
+						clear_store(name) {
+							return new Promise((resolve, reject) => {
+								let transaction = evt.target.result.transaction([name], 'readwrite');
+
+								transaction.addEventListener('error', evt => {
+									reject(evt.target.error);
+								});
+
+								transaction.objectStore(name).clear().addEventListener('success', evt => {
+									resolve();
+								});;
 							});
 						}
 					});
 				});
 
-				request.addEventListener('error', evt => {
+				db_request.addEventListener('error', evt => {
 					evt.preventDefault();
 					reject(evt.target.error);
 				});
@@ -105,201 +102,267 @@ export default function(name, version) {
 }
 
 class Store {
-	constructor(store, config) {
-		this[_store] = store;
-		this[_cfg] = config;
+	static with_transaction(name, cfg, transaction) {
+		let store = new this();
+		store['name'] = name;
+		store['cfg'] = cfg;
+		store['transaction'] = transaction;
+		return store;
+	}
+
+	static with_db(name, cfg, db) {
+		let store = new this();
+		store['name'] = name;
+		store['cfg'] = cfg;
+		store['db'] = db;
+		store['mode'] = 'readonly';
+		return store;
 	}
 
 	index(name) {
-		return new Index(this[_store].index(name));
+		let store = clone_object(this);
+		store['_index'] = name;
+		return store;
 	}
 
 	get(keys = []) {
-		return get_from_store(this[_store], this[_store].transaction, 'get', keys);
-	}
-
-	range(...args) {
-		return new Range(this[_store].transaction, this[_store], ...args);
-	}
-
-	add(...args) {
-		return update_store.call(this, 'add', ...args);
-	}
-
-	put(...args) {
-		return update_store.call(this, 'put', ...args);
-	}
-
-	delete(...args) {
-		return update_store.call(this, 'delete', ...args);
-	}
-
-	clear() {
-		return new Promise((resolve, reject) => {
-			add_transaction_handler(this[_store].transaction, reject);
-
-			this[_store].clear()
-				.addEventListener('success', evt => {
-					resolve();
+		return get_store_observer(this, (store, observer) => {
+			(Array.isArray(keys) ? keys : [keys])
+				.forEach(key => {
+					store.get(key)
+						.addEventListener('success', evt => {
+							observer.onNext(new Item(key, evt.target.result));
+						});
 				});
 		});
 	}
+
+	range(...args) {
+		return new Range(clone_object(this), ...args);
+	}
 }
 
-class Index {
-	constructor(index) {
-		this[_index] = index;
+class StoreMut extends Store {
+	static with_db(name, cfg, db) {
+		let store = super.with_db(name, cfg, db);
+		store['mode'] = 'readwrite';
+		return store;
 	}
 
-	get(keys = []) {
-		return get_from_store(this[_index], this[_index].objectStore.transaction, 'get', keys);
+	delete(key) {
+		return get_store_observer(this, (store, observer) => {
+			store.delete(key)
+				.addEventListener('success', evt => {
+					observer.onNext(new Item(key, null));
+				});
+		});
+	}
+
+	post(data, id = undefined) {
+		return get_store_observer(this, (store, observer) => {
+			id = add_new_id(data, id, this);
+
+			store.add(data, id)
+				.addEventListener('success', evt => {
+					observer.onNext(new Item(evt.target.result, data));
+				});
+		});
+	}
+
+	post_list(data_list) {
+		return get_store_observer(this, (store, observer) => {
+			data_list.forEach(([data, id]) => {
+				id = add_new_id(data, id, this);
+
+				store.add(data, id)
+					.addEventListener('success', evt => {
+						observer.onNext(new Item(evt.target.result, data));
+					});
+			});
+		});
+	}
+
+	put(data, id = undefined) {
+		return get_store_observer(this, (store, observer) => {
+			id = add_new_id(data, id, this);
+
+			store.put(data, id)
+				.addEventListener('success', evt => {
+					observer.onNext(new Item(evt.target.result, data));
+				});
+		});
+	}
+
+	put_list(data_list) {
+		return get_store_observer(this, (store, observer) => {
+			data_list.forEach(([data, id]) => {
+				id = add_new_id(data, id, this);
+
+				store.put(data, id)
+					.addEventListener('success', evt => {
+						observer.onNext(new Item(evt.target.result, data));
+					});
+			});
+		});
 	}
 
 	range(...args) {
-		return new Range(this[_index].objectStore.transaction, this[_index], ...args);
+		return new RangeMut(clone_object(this), ...args);
 	}
 }
 
 class Range {
-	constructor(transaction, store, type, ...args) {
-		this[_store] = store;
-		this[_trans] = transaction;
+	constructor(store, type, ...args) {
+		this['store'] = store;
 
 		switch ( type ) {
 			case 'only':
-				this[_range] = window.IDBKeyRange.only(...args);
+				this['range'] = window.IDBKeyRange.only(...args);
 				break;
 			case 'lower_bound':
-				this[_range] = window.IDBKeyRange.lowerBound(args.shift(), args.shift() || false);
+				this['range'] = window.IDBKeyRange.lowerBound(args.shift(), args.shift() || false);
 				break;
 			case 'upper_bound':
-				this[_range] = window.IDBKeyRange.upperBound(args.shift(), args.shift() || false);
+				this['range'] = window.IDBKeyRange.upperBound(args.shift(), args.shift() || false);
 				break;
 			case 'bound':
-				this[_range] = window.IDBKeyRange.bound(args.shift(), args.shift(), args.shift() || false, args.shift() || false);
+				this['range'] = window.IDBKeyRange.bound(args.shift(), args.shift(), args.shift() || false, args.shift() || false);
 				break;
 			default:
-				this[_range] = null;
+				this['range'] = null;
 		}
 	}
 
 	cursor(fn, direction = 'next') {
-		return new Promise((resolve, reject) => {
-			add_transaction_handler(this[_trans], reject, resolve);
-
-			this[_store].openCursor(this[_range], direction)
+		return get_store_observer(this['store'], (store, observer) => {
+			store.openCursor(this['range'], direction)
 				.addEventListener('success', evt => {
-					fn(evt.target.result || null, evt.target);
+					let cursor = evt.target.result;
+
+					if ( cursor !== null ) {
+						observer.observer.cursor = this['get_cursor'](cursor);
+
+						observer.onNext(new Item(cursor.primaryKey, cursor.value, cursor.key));
+
+						if ( evt.target.readyState !== 'pending' ) {
+							cursor.continue();
+						}
+					}
 				});
 		});
+	}
+
+	['get_cursor'](cursor) {
+		return {
+			continue: cursor.continue.bind(cursor),
+			advance: cursor.advance.bind(cursor)
+		};
 	}
 
 	count() {
-		return new Promise((resolve, reject) => {
-			add_transaction_handler(this[_trans], reject);
-
-			this[_store].count(this[_range])
+		return get_store_observer(this['store'], (store, observer) => {
+			store.count(this['range'])
 				.addEventListener('success', evt => {
-					resolve(evt.target.result);
+					observer.onNext(evt.target.result);
 				});
 		});
 	}
 }
 
-function update_store(action, items = []) {
-	return new Promise((resolve, reject) => {
-		let result,
-			key_path = this[_cfg].keyPath,
-			key_gen = this[_cfg].key_gen,
-			result = [];
+class RangeMut extends Range {
+	['get_cursor'](cursor) {
+		return {
+			continue: cursor.continue.bind(cursor),
+			advance: cursor.advance.bind(cursor),
+			update: cursor.update.bind(cursor),
+			delete: cursor.delete.bind(cursor)
+		};
+	}
+}
 
-		add_transaction_handler(this[_store].transaction, reject, resolve, result);
+class Item {
+	constructor(primary_key, data, key = null) {
+		this['_primary_key'] = primary_key;
+		this['data'] = data;
+		this['_key'] = key;
+	}
 
-		if ( action === 'delete' ) {
-			items
-				.forEach(key => {
-					this[_store][action](key).addEventListener('success', () => {
-						result.push(key);
-					});
-				});
-		} else if ( key_path === undefined && key_gen === undefined ) {
-			items
-				.forEach(item => {
-					this[_store][action](item.value, item.key).addEventListener('success', evt => {
-						resolve({key: evt.target.result, value: item.value});
-					});
-				});
-		} else if ( key_path === undefined && key_gen !== undefined ) {
-			items
-				.forEach(item => {
-					if ( item.key === undefined ) {
-						item.key = key_gen(item.value);
-					}
+	primary_key() {
+		return this['_primary_key'];
+	}
 
-					this[_store][action](item.value, item.key).addEventListener('success', evt => {
-						resolve({key: evt.target.result, value: item.value});
-					});
-				});
-		} else if ( key_path !== undefined && key_gen === undefined ) {
-			items
-				.forEach(item => {
-					this[_store][action](item).addEventListener('success', evt => {
-						item[evt.target.source.keyPath] = evt.target.result;
-						result.push(item);
-					});
-				});
-		} else if ( key_path !== undefined && key_gen !== undefined ) {
-			items
-				.forEach(item => {
-					if ( item[key_path] === undefined ) {
-						item[key_path] = key_gen(item);
-					}
-
-					this[_store][action](item).addEventListener('success', evt => {
-						item[evt.target.source.keyPath] = evt.target.result;
-						result.push(item);
-					});
-				});
+	key() {
+		if ( this['_key'] === null ) {
+			return this['_primary_key'];
 		}
-	});
-}
 
-function get_from_store(store, transaction, action, keys) {
-	return new Promise((resolve, reject) => {
-		let result = new Map();
-
-		add_transaction_handlen(transaction, reject, resolve, result);
-
-		keys
-			.forEach(key => {
-				if ( result.has(key) === false ) {
-					store[action](key)
-						.addEventListener('success', evt => {
-							if ( evt.target.result !== undefined ) {
-								result.set(key, evt.target.result);
-							} else {
-								result.set(key, null);
-							}
-						});
-				}
-			});
-	});
-}
-
-function add_transaction_handler(transaction, reject, resolve = null, result = null) {
-	transaction.addEventListener('error', evt => {
-		evt.preventDefault();
-		transaction.abort();
-		reject(evt.target.error);
-	});
-
-	if ( resolve !== null ) {
-		transaction.addEventListener('complete', () => {
-			if ( result !== null ) {
-				result = Stream(result);
-			}
-
-			resolve(result);
-		});
+		return this['_key'];
 	}
+
+	get(name) {
+		return this['data'][name];
+	}
+
+	has(name) {
+		return this['data'].hasOwnProperty(name);
+	}
+
+	empty() {
+		if ( this['data'] === null || this['data'] === undefined ) {
+			return true;
+		}
+
+		switch ( Object.prototype.toString.call(this['data']) ) {
+			case '[object Object]':
+				return (Object.keys(this['data']).length === 0);
+			case '[object Array]':
+				return (this['data'].length === 0);
+			case '[object Map]':
+			case '[object Set]':
+				return (this['data'].size === 0);
+		}
+
+		return false;
+	}
+
+	raw() {
+		return this['data'];
+	}
+}
+
+function get_store_observer(store, cb) {
+	return Rx.Observable.create(observer => {
+		let transaction = store['transaction'] || store['db'].transaction([store['name']], store['mode']),
+			store_obj = transaction.objectStore(store['name']);
+
+		if ( store['_index'] !== undefined ) {
+			store_obj = store_obj.index(store['_index']);
+		}
+
+		transaction.addEventListener('error', evt => {
+			evt.preventDefault();
+			transaction.abort();
+			observer.onError(evt.target.error);
+		});
+
+		transaction.addEventListener('complete', observer.onCompleted.bind(observer));
+
+		cb(store_obj, observer);
+	});
+}
+
+function add_new_id(data, id, store) {
+	if ( id === undefined && store['cfg'].keyType === 'uuid' ) {
+		if ( store['cfg'].keyPath === undefined ) {
+			id = uuid.v4();
+		} else if ( data[store['cfg'].keyPath] === undefined ) {
+			data[store['cfg'].keyPath] = uuid.v4();
+		}
+	}
+
+	return id;
+}
+
+function clone_object(obj) {
+	return Object.assign(new obj.constructor(), obj);
 }
